f0b533768c86e03f6feb115ccc2c47ecfc4d70d4
yasu <y.komaki1124@gmail.com>
2020-03-04 18:36:33 +0900

    vue側
    できてることはボタンコンポーネントにAPIたたかせてるまで

---
+++ view/src/components/Generator.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row align="center" id="gene">
+    <v-col class="text-center" cols="12" sm="12">
+      <div class="my-2">
+        <v-btn @click="generate" text large color="primary">Generate</v-btn>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+  <script>
+  export default {
+    name: 'Gene',
+    methods: {
+      generate() {
+        this.axios.get('https://httpbin.org/get')
+          .then((response) => {
+            alert("ぽけ"+response.data.origin);
+          })
+          .catch((e) => {
+            alert(e);
+          });
+      }
+    }
+  }
+</script>
