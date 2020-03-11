38cd3f35c3a1cd192e85dec4c3dbec84e6d5988a
yasu <y.komaki1124@gmail.com>
2020-03-11 19:39:27 +0900

    レスポンスのJSONを期待している構造体に入れてそれをカードに記載

---
+++ view/src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card class="mx-auto" max-width="344">
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="150px"
+    ></v-img>
+    <v-card-title>
+      {{ pokemon.name }}
+    </v-card-title>
+    <v-card-subtitle> 図鑑No.{{ pokemon.id }} </v-card-subtitle>
+  </v-card>
+</template>
+<script>
+export default {
+  data: () => ({
+    show: true
+  }),
+  props: {
+    pokemon: {
+      id: String,
+      name: String
+    }
+  }
+};
+</script>
--- view/src/components/Generator.vue
+++ view/src/components/Generator.vue
@@ -1,30 +1,50 @@
 <template>
-  <v-row align="center" id="gene">
-    <v-col class="text-center" cols="12" sm="12">
-      <div class="my-2">
-        <v-btn @click="generate" text large color="primary">Generate</v-btn>
-      </div>
-    </v-col>
-  </v-row>
+    <v-container>
+        <v-row align="center" id="gene">
+            <v-col class="text-center" cols="12" sm="12">
+                <div class="my-2">
+                    <v-btn @click="generate" text large color="primary">Generate</v-btn>
+                </div>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col
+            align="center"
+            cols="4"
+            v-for="(pokemon,key) in pokemons"
+            :key="key"
+            >
+            <Card :pokemon="pokemon"/>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
-  <script>
-  export default {
-    data () {
-      return {
-        pokemon: []
-      }
-    },
-    methods: {
-      generate() {
-        this.axios.get('https://httpbin.org/get')
-          .then((response) => {
-            this.msg=response.data.origin;
-          })
-          .catch((e) => {
-            alert(e);
-          });
-      }
-    }
-  }
+<script>
+    import Card from "./Card";
+    export default{
+        name:"Gene",
+        components:{
+            Card
+        },
+        data(){
+            return{
+                pokemons:[]
+            };
+        },
+        methods:{
+            generate(){
+                console.log("aa")
+                this.axios
+                .get("https://jsonplaceholder.typicode.com/users")
+                .then(response=>{
+                    this.pokemons=response.data;
+                    console.log(this.pokemons);
+                })
+                .catch(e=>{
+                    alert(e);
+                });
+            }
+        }
+    };
 </script>
